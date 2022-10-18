2633e2534b17765fc1000810f54d96afe13e6576
heiner urena <hurenaz@insservicios.com>
2022-10-18 16:54:59 -0600

    firt commit

---
+++ app/src/main/java/utn/practica/heiner/InicioActivity.kt
@@ -0,0 +1,93 @@
+package utn.practica.heiner
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import utn.practica.heiner.databinding.ActivityInicioBinding
+
+class InicioActivity : AppCompatActivity()
+{
+    //Objeto para generar acceso a los controles creados en la vista ...
+    private lateinit var  binding: ActivityInicioBinding
+
+
+    //objeto para realizar la comunicacion
+    private  lateinit var  auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityInicioBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //inicializo el objeto de autenticacion, realmente firebase
+        FirebaseApp.initializeApp(this)
+        auth = Firebase.auth
+
+        binding.btLogin.setOnClickListener { hacelogin() }
+        binding.btRegister.setOnClickListener { haceRegitro() }
+    }
+    private fun haceRegitro() {
+        val  correo = binding.etCorreo.text.toString()
+        val  clave = binding.etClave.text.toString()
+
+        auth.createUserWithEmailAndPassword(correo,clave).addOnCompleteListener(this){ task ->
+            if(task.isSuccessful){
+                val user = auth.currentUser
+                refresca(user)
+            }
+            else{ // si no hizo el registro....
+                Toast.makeText(baseContext,getString(R.string.tv_fallo), Toast.LENGTH_LONG).show()
+                refresca(null)
+
+            }
+        }
+    }
+
+    private fun refresca(user: FirebaseUser?) {
+        if(user != null){
+            // me paso a la pantalla principal .....
+            val intent = Intent(this,CentralActivity::class.java)
+
+            startActivity(intent)
+        }
+    }
+
+    private fun hacelogin() {
+        val  correo = binding.etCorreo.text.toString()
+        val  clave = binding.etClave.text.toString()
+
+        auth.signInWithEmailAndPassword(correo,clave).addOnCompleteListener(this){ task ->
+            if(task.isSuccessful){
+                val user = auth.currentUser
+                refresca(user)
+            }
+            else{ // si no hizo el registro....
+                Toast.makeText(baseContext,getString(R.string.tv_fallo), Toast.LENGTH_LONG).show()
+                refresca(null)
+
+            }
+        }
+    }
+
+    override fun onStart() //se ejecuta cuando la app esta en la pantalla
+    {//login automantico
+        super.onStart()
+        val usuario = auth.currentUser
+        refresca(usuario)
+    }
+
+
+}
