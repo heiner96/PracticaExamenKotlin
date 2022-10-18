ee2aa83dac509ca0973f9bfc2c9b04f7e0d36175
heiner urena <hurenaz@insservicios.com>
2022-10-18 17:50:17 -0600

    Login y Log off terminados

--- app/src/main/java/utn/practica/heiner/InicioActivity.kt
+++ app/src/main/java/utn/practica/heiner/InicioActivity.kt
@@ -4,12 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.navigation.NavigationView
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
